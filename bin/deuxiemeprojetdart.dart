84d1b57a2f93d70c881ad2f0356af4939b3d3858
Nsekuye Ziggy Romeon <54572604+Allyby@users.noreply.github.com>
2026-02-20 09:47:12 +0200

    first commit

---
+++ bin/deuxiemeprojetdart.dart
@@ -0,0 +1,5 @@
+import 'package:deuxiemeprojetdart/deuxiemeprojetdart.dart' as deuxiemeprojetdart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${deuxiemeprojetdart.calculate()}!');
+}
---
+++ test/deuxiemeprojetdart_test.dart
@@ -0,0 +1,8 @@
+import 'package:deuxiemeprojetdart/deuxiemeprojetdart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
